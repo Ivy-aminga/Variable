300ed1e2bc26ba73df715d66b8c4e18df56c8924
Ivy-aminga <amingaivy@gmail.com>
2023-02-03 09:31:23 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    var fullName = "Ivy Aminga"
+    println(fullName)
+    var age: Byte = 27
+    println(age)
+    var phoneNumber: String = "0712345678"
+    println(phoneNumber)
+    var weightKg = 55
+    println(weightKg)
+    var citizenshipAkenyan: Boolean = true
+    var citizenshipNotKenyan: Boolean = false
+    println(citizenshipAkenyan)
+    println(citizenshipNotKenyan)
+
+
+}
